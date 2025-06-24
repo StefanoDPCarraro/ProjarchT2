befdb6b43496df7a23f89c9a2e167ad1e784833f
Stefano Carraro <stefanodpc29@gmail.com>
2025-06-23 23:57:00 -0300

    Added component annotation

--- assignment/src/main/java/projarch/assignment/application/useCase/CadastraJogoUC.java
+++ assignment/src/main/java/projarch/assignment/application/useCase/CadastraJogoUC.java
@@ -1,9 +1,12 @@
 package projarch.assignment.application.useCase;
 
+import org.springframework.stereotype.Component;
+
 import projarch.assignment.adapters.IMapper.IJogosMapper;
 import projarch.assignment.application.dto.response.JogoDTO;
 import projarch.assignment.domain.services.JogosService;
 
+@Component
 public class CadastraJogoUC {
     private JogosService jogosService;
     private IJogosMapper jogosMapper;
