b02bf376adde069cc8df76b9a69e35ae251ef8a1
Stefano Carraro <stefanodpc29@gmail.com>
2025-06-23 23:29:23 -0300

    Added docker compose for database and unified controllers with the same path

--- assignment/src/main/java/projarch/assignment/adapters/controller/CadastroController.java
+++ assignment/src/main/java/projarch/assignment/adapters/controller/CadastroController.java
@@ -1,27 +1,31 @@
 package projarch.assignment.adapters.controller;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import projarch.assignment.application.dto.response.AluguelDTO;
+import projarch.assignment.application.dto.response.JogoDTO;
 import projarch.assignment.application.useCase.GetAllAlugueisUC;
+import projarch.assignment.application.useCase.GetAllJogosUC;
 import projarch.assignment.application.useCase.ValidaAluguelUC;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/acmegames/cadastro")
-public class AluguelController {
+public class CadastroController {
     private final GetAllAlugueisUC getAllAlugueisUC;
     private final ValidaAluguelUC validaAluguelUC;
+    private final GetAllJogosUC getAllJogosUC;
 
-    public AluguelController(GetAllAlugueisUC getAllAlugueisUC, ValidaAluguelUC validaAluguelUC){
+    public CadastroController(GetAllAlugueisUC getAllAlugueisUC, ValidaAluguelUC validaAluguelUC, GetAllJogosUC getAllJogosUC) {
         this.getAllAlugueisUC = getAllAlugueisUC;
         this.validaAluguelUC = validaAluguelUC;
+        this.getAllJogosUC = getAllJogosUC;
     }
 
     @GetMapping("/listaalugueis")
@@ -29,9 +33,14 @@ public class AluguelController {
         return getAllAlugueisUC.execute();
     }
 
-    @PostMapping("/validaaluguel")
-    public ResponseEntity<Boolean> validaAluguel(@RequestBody Integer id){
-        return ResponseEntity.ok(validaAluguelUC.execute(id));
+    @GetMapping("/validaaluguel")
+    public Boolean validaAluguel(@RequestBody Integer id) {
+        return validaAluguelUC.execute(id);
+    }
+
+    @GetMapping("/listajogos")
+    public List<JogoDTO> listaJogos(){
+        return getAllJogosUC.execute();
     }
     
 }
